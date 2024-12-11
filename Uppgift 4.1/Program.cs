0cb99a422d7f237e2485892f8d179db7c0f86a3e
alsa07007 <alsa07007@E-PW05CGR5>
2024-12-11 12:06:25 +0100

    Add project files.

---
+++ Uppgift 4.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 10; i <= 30; i++)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(250);
+            Console.Clear();
+        }
+        
+        for (int x = 200; x >= 180; x--)
+        {
+            Console.WriteLine(x);
+            Thread.Sleep(250);
+            Console.Clear();
+        }
+        
+        for (int h = 1000; h <= 1400; h+=50)
+        {
+            Console.WriteLine(h);
+            Thread.Sleep(250);
+            Console.Clear();
+        }
+    }
+}
